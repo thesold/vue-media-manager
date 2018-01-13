6e41f008959913ad6bf8893c10cf1167772301f8
Marco Mark <marco@silvanite.com>
2018-01-13 14:20:31 +0000

    Configuration files

---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+import RollupPluginVue from 'rollup-plugin-vue'
+import RollupPluginBabel from 'rollup-plugin-babel'
+
+export default {
+    input: 'src/index.js',
+    output: {
+        file: 'dist/index.js',
+        format: 'es',
+    },
+    external: [
+        'axios',
+        'vue2-dropzone',
+        'vue2-dropzone/dist/vue2Dropzone.css',
+    ],
+    plugins: [
+        RollupPluginVue(),
+        RollupPluginBabel(),
+    ],
+}
